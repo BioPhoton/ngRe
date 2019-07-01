d2933cb5eb9e7ae657d52b3a178452e419ef8046
Michael Hladky <michael@hladky.at>
2019-07-01 02:42:33 +0200

    update

--- src/app/push.pipe.ts
+++ src/app/push.pipe.ts
@@ -1,6 +1,6 @@
 import {ChangeDetectorRef, OnDestroy, Pipe, PipeTransform} from '@angular/core';
 import {isObservable, Observable, of, Subject} from 'rxjs';
-import {distinctUntilChanged, switchAll, takeUntil, tap} from 'rxjs/operators';
+import {distinctUntilChanged, map, switchAll, takeUntil, tap, withLatestFrom} from 'rxjs/operators';
 import {detectChanges} from './detectChanges';
 
 /**
@@ -17,19 +17,18 @@ import {detectChanges} from './detectChanges';
 @Pipe({name: 'push', pure: false})
 export class PushPipe implements PipeTransform, OnDestroy {
   private value: any = null;
-  private currentObs: Observable<any> | null = null;
 
-  ngOnDestroy$ = new Subject();
+  checkReference$$ = new Subject<boolean>();
+  ngOnDestroy$$ = new Subject<boolean>();
   observablesToSubscribe$ = new Subject<Observable<any>>();
 
   handleChangesSideEffect$ = this.observablesToSubscribe$.pipe(
-    // @TODO Minor improvement:
-    // @TODO We can't implement detecting changes of the incoming observable with distinctUntilChanged() operator here.
-    // @TODO (which could be good because we could get rid of this.currentObs !== obs)
-    // @TODO The reason for this is we compose the distinctUntilChanged()
-    // @TODO operator onto the observable before we next it on observablesToSubscribe$.
     // only forward new references (avoids holding a local reference to the previous observable => this.currentObs !== obs)
-    // distinctUntilChanged(),
+    distinctUntilChanged(),
+
+    // if onPush === true then check if value is referentially equal to previous
+    withLatestFrom(this.checkReference$$),
+    map(([o$, checkReference]) => checkReference ? o$.pipe(distinctUntilChanged()) : o$),
 
     // trigger change detection for new observables bound in the template
     detectChanges(this.cdRef),
@@ -41,46 +40,29 @@ export class PushPipe implements PipeTransform, OnDestroy {
     // assign value
     tap(v => this.value = v),
 
-    // trigger change detection for distinct <xvalues
+    // trigger change detection for distinct values
     detectChanges(this.cdRef),
 
     // unsubscribe if component gets destroyed
-    takeUntil(this.ngOnDestroy$)
+    takeUntil(this.ngOnDestroy$$)
   );
 
   constructor(private cdRef: ChangeDetectorRef) {
     this.handleChangesSideEffect$
-      .pipe(
-        takeUntil(this.ngOnDestroy$)
-      )
+      .pipe(takeUntil(this.ngOnDestroy$$))
       .subscribe();
   }
 
-  // @TODO Minor improvement: Use observable lifecycle hooks
+  // @TODO Minor improvement: Use observable lifecycle hooks over decorators
   ngOnDestroy(): void {
-    this.ngOnDestroy$.next(true);
+    this.ngOnDestroy$$.next(true);
   }
 
-  // @TODO Minor improvement: We could get rid of this line if we remove the onPush param
-  // @TODO And only forward new references
   transform<T>(obs: null | undefined, forwardOnlyNewReferences: boolean): null;
   transform<T>(obs: Observable<T>, forwardOnlyNewReferences: boolean): T;
-  transform(obs: Observable<any> | null | undefined, forwardOnlyNewReferences = true): any {
-    if (!isObservable(obs)) {
-      // @TODO Minor improvement: As discussed in handleChangesSideEffect$ we could get rid of this line
-      this.currentObs = of(null);
-      this.observablesToSubscribe$.next(this.currentObs);
-    } else {
-      // @TODO Minor improvement: As discussed in handleChangesSideEffect$ we could get rid of this line
-      if (this.currentObs !== obs) {
-        // @TODO Minor improvement: As discussed in handleChangesSideEffect$ we could get rid of this line
-        this.currentObs = obs;
-        // if onPush === true then check if value is referentially equal to previous
-        const distinctObs = forwardOnlyNewReferences ? obs.pipe(distinctUntilChanged()) : obs;
-        this.observablesToSubscribe$.next(distinctObs);
-      }
-    }
-
+  transform<T>(obs: Observable<T> | null | undefined, forwardOnlyNewReferences = true): T | null {
+    this.checkReference$$.next(forwardOnlyNewReferences);
+    this.observablesToSubscribe$.next(!isObservable(obs) ? of(null) : obs);
     return this.value;
   }
 }
